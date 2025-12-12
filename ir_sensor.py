8eca49681d902dc1f513f3feef170b81b4104046
elheuchert <elheuchert@gmail.com>
2025-12-12 13:10:04 -0800

    Delete ir_sensor.py

--- ir_sensor.py
+++
@@ -1,9 +0,0 @@
-from pyb import Pin, Timer, ADC
-## @brief add description here
-class IR_Sensor:
-
-    def __init__ (self, my_pin):
-        self.adc = ADC(my_pin)
-        self.black_ref = 0
-        self.white_ref = 0
-        self.calibrated = False
